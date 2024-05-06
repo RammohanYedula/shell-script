e2a3be47bb720f4db2736991e2846bec5d52575a
rammohan <rammohan8555@gmail.com>
2024-05-06 20:20:44 +0530

    hello world script added

--- 01-hello-world.sh
+++ 01-hello-world.sh
@@ -1,3 +1,7 @@
 #!/bin/bash  --> you should inform linux server,how to execute the script
 
 echo "Hello World, I am learning Devops with AWS"
+
+echo "Ramesh: Hi suresh, Good Morning"
+echo "Suresh: Good morning"
+
