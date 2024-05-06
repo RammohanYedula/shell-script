67c61c485e82a7c451ba45439caca638d092ae22
rammohan <rammohan8555@gmail.com>
2024-05-06 20:27:50 +0530

    hello world script added

--- 01-hello-world.sh
+++ 01-hello-world.sh
@@ -1,7 +1,11 @@
 #!/bin/bash  --> you should inform linux server,how to execute the script
 
 echo "Hello World, I am learning Devops with AWS"
+PERSON1=Ramesh
+PERSON2=Suresh
 
-echo "Ramesh: Hi suresh, Good Morning"
-echo "Suresh: Good morning"
+echo "$PERSON1: Hi $PERSON2, Good Morning"
+echo "$PERSON2: Good morning, $PERSON1"
+echo "$PERSON1: HOW ARE YOU $$PERSON2"
+
 
