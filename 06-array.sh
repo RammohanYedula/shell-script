13a010200aaccc99a720ec4460aaf69057a2f0e6
rammohan <rammohan8555@gmail.com>
2024-05-06 21:34:12 +0530

    hello world script added

---
+++ 06-array.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+
+# this is called array, instead of single value it can hold multiple values
+PERSONS=("Ram" "suresh"  "Mohan")
+
+# INSIDE array we always refer values woth index and it start from 0
+
+echo "First Person: ${PERSONS[0]}"
