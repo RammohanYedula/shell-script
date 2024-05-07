007a185a3635db615596c2f38572f4279e4f0b79
rammohan <rammohan8555@gmail.com>
2024-05-07 21:35:47 +0530

    added

--- 07-condition.sh
+++ 07-condition.sh
@@ -6,7 +6,7 @@ NUMBER=$1
 
 if [ $NUMBER -gt 10 ]
 then
-   echo "$NUMBER IS greater than 10"
-elese
-   echo "$NUMBER is not greater than 10"
-fi 
+    echo "$NUMBER is greater than 10"
+else
+    echo "$NUMBER is not greater than 10"
+fi
