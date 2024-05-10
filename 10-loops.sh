4508345e538c34b72bcc1fcbec2e9166213df939
rammohan <rammohan8555@gmail.com>
2024-05-10 15:41:00 +0530

    added

--- 10-loops.sh
+++ 10-loops.sh
@@ -12,6 +12,10 @@
 
 #########
 
+DATE=$(date %F)
+LOGSDIR=/home/centos/shellscript-logs
+SCRIPT_NAME=$0
+LOGFILE=$LOGSDIR/$0-$DATE.log
 
 
 USERID=$(id -u)
@@ -27,9 +31,28 @@ then
     exit 1
 fi
 
+VALIDATE(){
+    if [ $1 -ne 0 ]
+    then
+        echo -e "Installing $2 ...$R FAILURE $N"
+        exit 1
+    else
+        echo -e "Installing $2 ...$G SUCCESS $N"
+
+    fi 
+}
+
 for i in $@
 do 
-    yum install $i -y
+    yum list installed $i 
+    if [ $? -ne 0]
+    then
+        echo "$i is not installed, lets install it"
+        yum install $i -y &>>$LOGFILE
+        VALIDATE $? "$i"
+    else
+        echo -e "$Y $i is already installed"
+    fi 
 done
 
 
