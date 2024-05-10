a4d19534f91f29ef84d5083a03dbbcc5a95b37da
rammohan <rammohan8555@gmail.com>
2024-05-10 15:18:02 +0530

    added

--- 10-loops.sh
+++ 10-loops.sh
@@ -4,8 +4,32 @@
 #echo 2
 #echo 3
 
-for i in {1..20}
-do
-    echo $i 
+#for i in {1..20}
+#do
+ #   echo $i 
+#done
+
+
+#########
+
+
+
+USERID=$(id -u)
+R="\e[31m"
+G="\e[32m"
+N="\e[0m]"
+Y="\e[33m"
+
+
+if [ $USERID -ne 0 ]
+then
+    echo -e "$R Error: please run this script with root access $N"
+    exit 1
+fi
+
+for i in $@
+do 
+    yum install $i -y
 done
+
 
